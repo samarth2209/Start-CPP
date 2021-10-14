9e1858c8810020e99b0cbb954446d15458f59054
Rajshikhar <rajshikharpandey0023@gmail.com>
2021-10-14 10:21:24 +0530

    211265_task-2

---
+++ submissions/211265_task-2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //print memory size of bool
+    cout << "Size of bool: " << sizeof(bool) << "byte" << endl;
+    
+    // print memory size of char
+    cout << "Size of char: " << sizeof(char) << "byte" << endl;
+
+    // print memory size of int
+    cout << "Size of int: " << sizeof(int) << "bytes" << endl;
+
+    // print memory size of float
+    cout << "Size of float: " << sizeof(float) << "bytes" << endl;
+
+    // print memory size of double
+    cout << "Size of double: " << sizeof(double) << "bytes" <<endl;
+
+    return 0;
+}
