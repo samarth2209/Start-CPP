2376c787cbf95bd2273b1062e296ebbd6d08f4aa
NK-01 <72823658+NK-01@users.noreply.github.com>
2021-10-14 10:21:44 +0530

    Create 1914628_task2.cpp

---
+++ submissions/1914628_task2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Size of boolean data type : "<<sizeof(bool)<<" byte\n";
+    cout<<"Size of char data type : "<<sizeof(char)<<" byte\n";
+    cout<<"Size of int data type : "<<sizeof(int)<<" byte\n";
+    cout<<"Size of floating point data type : "<<sizeof(float)<<" byte\n";
+    cout<<"Size of double floating point data type : "<<sizeof(double)<<" byte\n";
+    return 0;
+}
+
