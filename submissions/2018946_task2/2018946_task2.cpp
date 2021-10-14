316447eb186aed654eaeae791ee8e11b219ff968
Madhav Gupta <iamadhavgupta07@gmail.com>
2021-10-14 20:37:42 +0530

    task_2

---
+++ submissions/2018946_task2/2018946_task2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<sizeof(short int)<<endl;//size of short int
+    cout<<sizeof(int)<<endl;//size of int    
+    cout<<sizeof(double)<<endl;//size of double
+    cout<<sizeof(float)<<endl;//size of float 
+    cout<<sizeof(bool);//size of bool
+    return 0;
+}
