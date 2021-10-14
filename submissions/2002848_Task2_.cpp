af40458e9fc37fef5e6dd560e2886a5d736713cf
Harshil <harshilsharmaa0@gmail.com>
2021-10-13 21:29:44 -0800

    Task 2 CPP

---
+++ submissions/2002848_Task2_.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout<<"Memory size of int : "<<sizeof(int)<<endl;
+    cout<<"Memory size of float : "<<sizeof(float)<<endl;
+    cout<<"Memory size of double : "<<sizeof(double)<<endl;
+    cout<<"Memory size of char : "<<sizeof(char)<<endl;
+    cout<<"Memory size of boolean : "<<sizeof(bool)<<endl;
+
+
+    
+    return 0;
+}
