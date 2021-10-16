68dca5c178e34829fed97febf16f28896c138dec
pk002841 <92570798+pk002841@users.noreply.github.com>
2021-10-16 22:29:16 +0530

    Create 1902432_task2.cpp

---
+++ submissions/1902432_task2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<"Memory size of int is : "<<sizeof(int)<<endl;
+    cout<<"Memory size of char is : "<<sizeof(char)<<endl;
+    cout<<"Memory size of floatis : "<<sizeof(float)<<endl;
+    cout<<"Memory size of bool is: "<<sizeof(bool)<<endl;
+    cout<<"Memory size of double is: "<<sizeof(double)<<endl;
+    return 0;
+}
