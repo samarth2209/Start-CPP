042046444c69c619d821112fcfc97692f8be4e46
pawangupta5167 <52108857+pawangupta5167@users.noreply.github.com>
2021-10-22 08:05:44 -0700

    Create Task2.cpp

---
+++ submissions/1911126_Pawangupta5167/Task2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   cout << "Size of char data type : " << sizeof(char) << " byte" << endl;
+   cout << "Size of int data type : " << sizeof(int) << " byte" << endl;
+   cout << "Size of unsigned int data type : " << sizeof(unsigned int) << " byte" << endl;
+   cout << "Size of floating point data type : " << sizeof(float) << " byte" << endl;
+   cout << "Size of double floating point data type : " << sizeof(double) << " byte" << endl;
+   return 0;
+}
