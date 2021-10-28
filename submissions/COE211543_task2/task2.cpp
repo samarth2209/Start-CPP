6364a0d70a611b0710d2effe58f55f884a42b366
Vanshika <vanshikachanana29@gmail.com>
2021-10-28 18:27:27 +0530

    task2.cpp completed

---
+++ submissions/COE211543_task2/task2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std ; 
+
+int main(){
+    //Enter the data types .. 
+    cout << "SIze of any 5 data types are " << endl ;
+    //size of int datatype 
+    cout << "size of int " << sizeof(int) << " bytes"<< endl ;
+    //size of short int datatype 
+    cout << "size of int " << sizeof(short int ) <<" bytes" <<  endl ;
+    //size of signed char datatype
+    cout << "size of int " << sizeof(signed char) << " bytes" << endl ;
+    //size of long int datatype 
+    cout << "size of int " << sizeof(long int ) << " bytes" <<  endl ;
+    //size of long long int datatype 
+    cout << "size of int " << sizeof(long long int) << " bytes" << endl ;
+}
