f620c11cc4050cfceb22cff7390f9c719bf0957a
amisha rani <83347105+AmishaRani@users.noreply.github.com>
2021-10-30 14:21:24 +0530

    task2 done
    Size of Datatype

---
+++ submissions/2002362_task2/Task2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Size of int is "<< sizeof(int)<< " bytes\n";   
+    cout<<"Size of char is "<< sizeof(char)<< " byte\n";   
+    cout<<"Size of float is "<< sizeof(float)<< " bytes\n";   
+    cout<<"Size of double is "<< sizeof(double)<< " bytes\n";   
+    cout<<"Size of short int is "<< sizeof(short int)<< " bytes\n";   
+    return 0;
+}
