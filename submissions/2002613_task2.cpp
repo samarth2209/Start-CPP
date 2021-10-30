b37f31f521969bdf8879336cbfebec4772ac67d2
samarth2209 <93388133+samarth2209@users.noreply.github.com>
2021-10-30 20:48:48 +0530

    Create 2002613_task2.cpp

---
+++ submissions/2002613_task2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<sizeof(short int)<<endl;
+    cout<<sizeof(int)<<endl;
+    cout<<sizeof(double)<<endl;
+    cout<<sizeof(float)<<endl;
+    cout<<sizeof(bool);
+    return 0;
+}
